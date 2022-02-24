d199cdb188e09a67d1f196c5e3f67dbdd44722de
Sidany Walker <sidanyk@gmail.com>
2022-02-24 11:21:35 -0500

    Initial Commit

---
+++ MyInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyInstagram
+//
+//  Created by Sidany Walker on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
